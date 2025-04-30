81d8a4eb7ee355d3049ef621ebc6ee3abb20e9a4
Anthony Barragan <anthony.bar.89@gmail.com>
2025-04-30 13:44:48 -0700

    added manifest for download

---
+++ app/manifest.ts
@@ -0,0 +1,47 @@
+// {
+//   "name": "MyWebSite",
+//   "short_name": "MySite",
+//   "icons": [
+//     {
+//       "src": "/web-app-manifest-192x192.png",
+//       "sizes": "192x192",
+//       "type": "image/png",
+//       "purpose": "maskable"
+//     },
+//     {
+//       "src": "/web-app-manifest-512x512.png",
+//       "sizes": "512x512",
+//       "type": "image/png",
+//       "purpose": "maskable"
+//     }
+//   ],
+//   "theme_color": "#ffffff",
+//   "background_color": "#ffffff",
+//   "display": "standalone"
+// }
+
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Next.js PWA',
+    short_name: 'NextPWA',
+    description: 'A Progressive Web App built with Next.js',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#1B242D',
+    icons: [
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
