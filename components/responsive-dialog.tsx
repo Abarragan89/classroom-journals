197b4e6787c23f93d811177733f83b361fbc8058
Anthony Barragan <anthony.bar.89@gmail.com>
2025-05-01 09:55:40 -0700

    changed environment variables to send from math-fact-missions

--- components/responsive-dialog.tsx
+++ components/responsive-dialog.tsx
@@ -1,26 +1,12 @@
 import * as React from 'react';
 
-// import { Button } from '@/components/ui/button';
 import {
     Dialog,
     DialogContent,
     DialogDescription,
     DialogHeader,
     DialogTitle,
-    // DialogTrigger,
 } from '@/components/ui/dialog';
-// import {
-//     Drawer,
-//     DrawerClose,
-//     DrawerContent,
-//     // DrawerDescription,
-//     DrawerFooter,
-//     DrawerHeader,
-//     DrawerTitle,
-//     // DrawerTrigger,
-// } from '@/components/ui/drawer';
-
-// import { useMediaQuery } from '@/hooks/use-media-query';
 
 export function ResponsiveDialog({
     children,
@@ -35,9 +21,6 @@ export function ResponsiveDialog({
     title: string;
     description?: string;
 }) {
-    // const isDesktop = useMediaQuery('(min-width: 768px)');
-
-    // if (isDesktop) {
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogContent onClick={(e) => e.stopPropagation()} onKeyDown={(e) => e.stopPropagation()} onPointerMove={(e) => e.stopPropagation()}
@@ -52,22 +35,4 @@ export function ResponsiveDialog({
             </DialogContent>
         </Dialog>
     );
-    // }
-
-    // return (
-    //     <Drawer open={isOpen} onOpenChange={setIsOpen}>
-    //         <DrawerContent tabIndex={0}>
-    //             <DrawerHeader className="text-left">
-    //                 <DrawerTitle>{title}</DrawerTitle>
-    //                 {description && <DialogDescription>{description}</DialogDescription>}
-    //             </DrawerHeader>
-    //             {children}
-    //             <DrawerFooter className="pt-2">
-    //                 <DrawerClose asChild>
-    //                     <Button variant="outline">Cancel</Button>
-    //                 </DrawerClose>
-    //             </DrawerFooter>
-    //         </DrawerContent>
-    //     </Drawer>
-    // );
 }
