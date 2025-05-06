6d2cf00d06a9a445ee5e91de6dae2fde8694e32e
Anthony Barragan <anthony.bar.89@gmail.com>
2025-05-05 22:40:55 -0700

    different padding for different screen sizes

--- components/shared/prompt-response-editor/editor.tsx
+++ components/shared/prompt-response-editor/editor.tsx
@@ -124,7 +124,7 @@ export default function Editor({
                 onCopy={(e) => e.preventDefault()}
                 onCut={(e) => e.preventDefault()}
                 className={`
-                    bg-transparent outline-border border border-border font-mono shadow-border shadow-[inset_0px_0px_10px_0px_rgba(0,_0,_0,_0.1)] p-4 md:p-8 textarea-field-size-content
+                    bg-transparent outline-border border border-border font-mono shadow-border shadow-[inset_0px_0px_10px_0px_rgba(0,_0,_0,_0.1)] p-4 md:p-7 textarea-field-size-content
                     ${jotType === 'BLOG' ? 'min-h-48' : ''}
                 `}
                 value={journalText}
