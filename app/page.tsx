46e4ecaef17e5ad1c4131db2bedd5adb7494d9d9
Anthony Barragan <anthony.bar.89@gmail.com>
2025-05-09 07:51:45 -0700

    added three more categories of photos

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ Aliquam bibendum sapien id magna congue, nec fermentum velit convallis. In volut
                     <div className="flex flex-col items-center">
                         <h5 className="font-bold text-lg mb-1">Benefits</h5>
                         <div className="text-primary space-y-2">
-                            <p>- Cannot copy and paste text into the editor (TRY IT!)</p>
+                            <p>- CANNOT copy and paste text into the editor (TRY IT!)</p>
                             <p>- No distractions with font, font-sizes, formatting, images, etc.</p>
                             <p>- Word predication will not complete sentences</p>
                             <p>- Grammar or spelling mistakes will not be highlighted</p>
