82326df6bb610717f8ebcf774b186c97b27209aa
Anthony Barragan <anthony.bar.89@gmail.com>
2025-05-17 18:46:00 -0700

    updated middle

--- middleware.ts
+++ middleware.ts
@@ -61,6 +61,8 @@ export async function middleware(request: NextRequest) {
         const loginUrl = new URL("/", request.url);
         return NextResponse.redirect(loginUrl);
     }
+
+    return response
 }
 
 export const config = {
