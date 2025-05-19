532b4aec62c923a867dd0f34f5256ac44588e01b
Anthony Barragan <anthony.bar.89@gmail.com>
2025-05-19 09:59:01 -0700

    lightened background for tech

--- app/(student)/student-grades/page.tsx
+++ app/(student)/student-grades/page.tsx
@@ -18,7 +18,6 @@ export default async function StudentGrades() {
     console.log('classroomId', classroomId)
 
 
-
     return (
         <>
             <Header session={session} studentId={studentId} />
