eddbde85dcc20296f685f4153cbad2eafc1fb1aa
Anthony Barragan <anthony.bar.89@gmail.com>
2025-05-29 21:31:51 -0700

    erased old client checker

--- components/absent-user-checker.tsx
+++ components/absent-user-checker.tsx
@@ -1,38 +1,3 @@
-// "use client";
-
-// import { useEffect } from "react";
-// import { usePathname } from "next/navigation";
-
-// const MAX_VISITS = 5;
-// // Add a callBackUrl so it goes back to the exact same page
-// const REDIRECT_URL = "https://abarragan89.github.io/jotter-blog-still-there/";
-
-// export default function AbsentUserChecker() {
-//     const pathname = usePathname();
-
-//     useEffect(() => {
-//         const lastPath = localStorage.getItem("lastPath");
-//         const visitCountStr = localStorage.getItem("visitCount");
-//         const visitCount = visitCountStr ? parseInt(visitCountStr) : 0;
-
-//         if (lastPath === pathname) {
-//             const newCount = visitCount + 1;
-//             localStorage.setItem("visitCount", newCount.toString());
-
-//             if (newCount >= MAX_VISITS) {
-//                 localStorage.removeItem("visitCount");
-//                 localStorage.removeItem("lastPath");
-//                 window.location.href = REDIRECT_URL; // full-page redirect
-//             }
-//         } else {
-//             localStorage.setItem("lastPath", pathname);
-//             localStorage.setItem("visitCount", "1");
-//         }
-//     }, [pathname]);
-
-//     return null; // no UI
-// }
-
 "use client";
 
 import { useEffect } from "react";
