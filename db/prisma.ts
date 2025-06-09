af1f5024e383b65d8ae0581ae9a31713e605a18f
Anthony Barragan <anthony.bar.89@gmail.com>
2025-06-08 19:55:46 -0700

    updated package json for vps and added dockerfiles which will not be used because using coolify

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,40 +1,15 @@
-// import { Pool, neonConfig } from '@neondatabase/serverless'
-// import { PrismaNeon } from '@prisma/adapter-neon'
-// import { PrismaClient } from '@prisma/client'
-// import ws from 'ws'
-
-// neonConfig.webSocketConstructor = ws
-// const connectionString = `${process.env.DATABASE_URL}`
-
-// const pool = new Pool({ connectionString })
-// const adapter = new PrismaNeon(pool)
-// export const prisma = new PrismaClient({ adapter })
-
-
-// lib/db.ts (or wherever your DB connection is set up)
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
-
-const connectionString = process.env.DATABASE_URL!
-
 declare global {
     var prisma: PrismaClient | undefined
 }
 
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaNeon({ connectionString });
+const localPrism = global.prisma || new PrismaClient({ adapter });
+if (process.env.NODE_ENV === 'development') global.prisma = localPrism;
 
-let prisma: PrismaClient
-
-if (!globalThis.prisma) {
-    const pool = new Pool({ connectionString })
-    const adapter = new PrismaNeon(pool)
-    prisma = new PrismaClient({ adapter })
-    globalThis.prisma = prisma
-} else {
-    prisma = globalThis.prisma
-}
-
-export { prisma }
+export const prisma = localPrism;
