0e10b4bb43ab50f876b4ec43b87ea45c704bf7a5
Anthony Barragan <anthony.bar.89@gmail.com>
2025-08-15 20:47:07 -0700

    added public icons expected by the browser for pwa and manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,22 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#1B242D',
     icons: [
       {
-        src: '/web-app-manifest-192x192.png',
+        src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/web-app-manifest-512x512.png',
+        src: '/icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png', 
         sizes: '512x512',
         type: 'image/png',
       },
