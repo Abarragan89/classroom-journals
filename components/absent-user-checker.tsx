e9da8268661e830827a5a6bf144ca973ae41737c
Anthony Barragan <anthony.bar.89@gmail.com>
2025-08-18 10:37:56 -0700

    updated absent check to pass the return url

--- components/absent-user-checker.tsx
+++ components/absent-user-checker.tsx
@@ -1,20 +1,67 @@
+// "use client"
+// import { useEffect, useState } from 'react'
+// import { useIdleTimer } from 'react-idle-timer'
+
+// export default function AbsentUserChecker() {
+//     const REDIRECT_URL = "https://abarragan89.github.io/jotter-blog-still-there/";
+//     const [state, setState] = useState<string>('Active')
+//     const [count, setCount] = useState<number>(0)
+//     const [remaining, setRemaining] = useState<number>(120000)
+
+//     const onIdle = () => {
+//         setState('Idle')
+//     }
+
+//     const onActive = () => {
+//         if (state) { }
+//         setState('Active')
+//     }
+
+//     const onAction = () => {
+//         setCount(count + 1)
+//     }
+
+//     const { getRemainingTime } = useIdleTimer({
+//         onIdle,
+//         onActive,
+//         onAction,
+//         timeout: 600_000,
+//         throttle: 1000
+//     })
+
+//     useEffect(() => {
+//         const interval = setInterval(() => {
+//             const secs = Math.ceil(getRemainingTime() / 1000);
+//             if (secs !== remaining) {
+//                 setRemaining(secs);
+//             }
+//             if (secs <= 0) {
+//                 window.location.href = REDIRECT_URL;
+//             }
+//         }, 1000);
+//         return () => clearInterval(interval);
+//     }, [remaining]);
+
+//     return null
+// }
+
 "use client"
-import { useEffect, useState } from 'react'
-import { useIdleTimer } from 'react-idle-timer'
+import { useEffect, useState } from "react"
+import { useIdleTimer } from "react-idle-timer"
 
 export default function AbsentUserChecker() {
-    const REDIRECT_URL = "https://abarragan89.github.io/jotter-blog-still-there/";
-    const [state, setState] = useState<string>('Active')
+    const REDIRECT_URL = "https://abarragan89.github.io/jotter-blog-still-there/"
+    const [state, setState] = useState<string>("Active")
     const [count, setCount] = useState<number>(0)
     const [remaining, setRemaining] = useState<number>(120000)
 
     const onIdle = () => {
-        setState('Idle')
+        setState("Idle")
     }
 
     const onActive = () => {
         if (state) { }
-        setState('Active')
+        setState("Active")
     }
 
     const onAction = () => {
@@ -25,22 +72,28 @@ export default function AbsentUserChecker() {
         onIdle,
         onActive,
         onAction,
-        timeout: 600_000,
-        throttle: 1000
+        // timeout: 600_000, // 10 min
+        timeout: 60, // 10 min
+        throttle: 1000,
     })
 
     useEffect(() => {
         const interval = setInterval(() => {
-            const secs = Math.ceil(getRemainingTime() / 1000);
+            const secs = Math.ceil(getRemainingTime() / 1000)
             if (secs !== remaining) {
-                setRemaining(secs);
+                setRemaining(secs)
             }
             if (secs <= 0) {
-                window.location.href = REDIRECT_URL;
+                const currentUrl = window.location.href
+                const redirectUrl = `${REDIRECT_URL}?returnUrl=${encodeURIComponent(
+                    currentUrl
+                )}`
+                window.location.href = redirectUrl
             }
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [remaining]);
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [remaining, getRemainingTime])
 
     return null
 }
