84b7bd920569d6766b42abee01d32634be544f02
Anthony Barragan <anthony.bar.89@gmail.com>
2026-03-27 01:23:38 -0700

    triggering a redeployment

--- app/api/metrics/route.ts
+++ app/api/metrics/route.ts
@@ -4,11 +4,17 @@ import client from 'prom-client';
 const registry = new client.Registry();
 client.collectDefaultMetrics({ register: registry });
 
-export async function GET() {
-    const metrics = await registry.metrics();
-    return new NextResponse(metrics, {
-        headers: {
-            'Content-Type': registry.contentType,
-        },
-    });
+export async function GET(request: Request) {
+  const token = request.headers.get('authorization');
+  
+  if (token !== `Bearer ${process.env.METRICS_SECRET}`) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  const metrics = await registry.metrics();
+  return new NextResponse(metrics, {
+    headers: {
+      'Content-Type': registry.contentType,
+    },
+  });
 }
